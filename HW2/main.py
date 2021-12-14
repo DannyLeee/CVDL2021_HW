7ec64de75e5d82a236ab20d61f2c305ba5d04f7e
DannyLeee <dannylee94049@gmail.com>
2021-12-14 15:48:43 +0800

    [FEAT] Q4.2 complete

--- HW2/main.py
+++ HW2/main.py
@@ -19,12 +19,14 @@ class Main(QMainWindow, ui.Ui_MainWindow):
 
         # link button click function
         self.Q4_1.clicked.connect(self.img_reconstruct)
+        self.Q4_2.clicked.connect(self.reconstruct_err)
 
         # load image
         self.Q4_img = []
         for i in range(1, 31):
             img = cv2.imread(f"Dataset/Q4_Image/{i}.jpg")
             self.Q4_img += [img]
+        self.Q4_img_hat = []
 
     # Q 4.1
     def img_reconstruct(self):
@@ -40,6 +42,7 @@ class Main(QMainWindow, ui.Ui_MainWindow):
         x_hat = np.dot(pca.transform(x)[:, :n_comp], pca.components_[:n_comp, :])
         x_hat += mu
         x_hat = x_hat.reshape(30, 400, 400, 3).astype(int)
+        self.Q4_img_hat = [x_hat[i] for i in range(30)]
 
         plt.figure(1)
         for idx, img in enumerate(self.Q4_img):
@@ -57,6 +60,20 @@ class Main(QMainWindow, ui.Ui_MainWindow):
                 plt.axis("off")
             imshow(x_hat[idx][:, :, ::-1])
         plt.show()
+
+    # Q 4.2
+    def reconstruct_err(self):
+        for i in range(30):
+            cv2.normalize(self.Q4_img_hat[i], self.Q4_img_hat[i], alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+
+        RE = []
+        for i in range(30):
+            gray = cv2.cvtColor(self.Q4_img[i], cv2.COLOR_RGB2GRAY)
+            gray_hat = cv2.cvtColor(self.Q4_img_hat[i].astype(np.uint8), cv2.COLOR_RGB2GRAY)
+            RE += [(gray - gray_hat).sum()]
+
+        print("Reconstruction Error:")
+        print(RE)
 
 
 if __name__ == '__main__':
