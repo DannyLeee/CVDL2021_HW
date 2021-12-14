fa25c34613ce15421545b7bea92a1fabeb533f6b
DannyLeee <dannylee94049@gmail.com>
2021-12-14 15:12:45 +0800

    [ADD] main.py

---
+++ HW2/main.py
@@ -0,0 +1,23 @@
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import cv2
+
+import ui
+
+
+class Main(QMainWindow, ui.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = Main()
+    main_window.show()
+    sys.exit(app.exec_())
